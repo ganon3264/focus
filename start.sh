bc7267e2ebef15e8ee9bfd9f4ccfc9793b2ca8d6
ganon3264 <twojstary6996@gmail.com>
2026-07-18 23:41:50 +0200

    check if python&uv are installed

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+FOCUS_DEBUG=1
 set -euo pipefail
+
+command -v python >/dev/null 2>&1 || { echo >&2 "Error: python is not installed or not on PATH."; exit 1; }
+command -v uv >/dev/null 2>&1 || { echo >&2 "Error: uv is not installed or not on PATH."; exit 1; }
 
 ./vendor-sync.py --check || ./vendor-sync.py
 
